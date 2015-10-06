e9e34ce56cb3f86c3b28deef0d07ff2a14d1b847
andela.kedeh <kingsley.edeh@andela.co>
2015-10-06 16:16:45 +0100

    added the code for StandingDBHelper class and all test passed at this point

--- app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingDBHelper.java
+++ app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingDBHelper.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.checkpoint4.wecking.standingstillapp.StandingContract;
-
 /**
  * Created by andela on 10/6/15.
  */
