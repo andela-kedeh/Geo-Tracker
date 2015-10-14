c459bfa39300d89e8d6187803f7256d1690d2463
andela.kedeh <kingsley.edeh@andela.co>
2015-10-14 14:31:02 +0100

    refactored data package to one table

--- app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingContract.java
+++ app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingContract.java
@@ -14,45 +14,19 @@ public class StandingContract {
 
     public static final String CONTENT_AUTHORITY = "com.checkpoint.wecking.standingstillapp";
 
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-
     public static final String PATH_STANDING = "standing";
-    public static final String PATH_LOCATION = "location";
 
     public static long normalizeDate(long startDate){
         Time time = new Time();
         time.setToNow();
-
         int julianDay = Time.getJulianDay(startDate, time.gmtoff);
 
         return time.setJulianDay(julianDay);
     }
 
-    public static final class LocationEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
-
-
-        public static final String TABLE_NAME = "location";
-        public static final String COLUMN_CITY_NAME = "city_name";
-        public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String COLUMN_COORD_LONG = "coord_long";
-
-        public static Uri buildLocationUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-        }
-    }
-
     public static final class StandingEntry implements BaseColumns {
-
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_STANDING).build();
+        public static final String URL = "content://" + CONTENT_AUTHORITY + "/" + PATH_STANDING;
+        public static final Uri CONTENT_URI = Uri.parse(URL);
 
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_STANDING;
@@ -71,40 +45,15 @@ public class StandingContract {
         public static final String COLUMN_STANDING_TIME = "standing_time";
         public static final String COLUMN_SET_RECORD_TIME = "set_record_time";
 
-        public static Uri buildWeatherUri(long id) {
+        public static final String COLUMN_STREET_NAME = "street";
+        public static final String COLUMN_STATE_NAME = "state";
+        public static final String COLUMN_COUNTRY_NAME = "country";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildStandingUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
-
-        public static Uri buildStandingLocation(String string) {
-            return CONTENT_URI.buildUpon().appendPath(string).build();
-        }
-
-        public static Uri buildStandingLocationWithStartDate(
-                String string, long startDate) {
-            long normalizedDate = normalizeDate(startDate);
-            return CONTENT_URI.buildUpon().appendPath(string)
-                    .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate)).build();
-        }
-
-        public static Uri buildStandingLocationWithDate(String string, long date) {
-            return CONTENT_URI.buildUpon().appendPath(string)
-                    .appendPath(Long.toString(normalizeDate(date))).build();
-        }
-
-        public static String getLocationStringFromUri(Uri uri) {
-            return uri.getPathSegments().get(1);
-        }
-
-        public static long getDateFromUri(Uri uri) {
-            return Long.parseLong(uri.getPathSegments().get(2));
-        }
-
-        public static long getStartDateFromUri(Uri uri) {
-            String dateString = uri.getQueryParameter(COLUMN_DATE);
-            if (null != dateString && dateString.length() > 0)
-                return Long.parseLong(dateString);
-            else
-                return 0;
-        }
     }
+
 }
--- app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingDBHelper.java
+++ app/src/main/java/com/checkpoint4/wecking/standingstillapp/data/StandingDBHelper.java
@@ -9,9 +9,9 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class StandingDBHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 5;
 
-    static final String DATABASE_NAME = "weather.db";
+    static final String DATABASE_NAME = "Standing.db";
 
     public StandingDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,41 +20,28 @@ public class StandingDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + StandingContract.LocationEntry.TABLE_NAME + " (" +
-                StandingContract.LocationEntry._ID + " INTEGER PRIMARY KEY," +
-                StandingContract.LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL," +
-                StandingContract.LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL," +
-                StandingContract.LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL" +
-                " );";
-
         final String SQL_CREATE_STANDING_TABLE = "CREATE TABLE " + StandingContract.StandingEntry.TABLE_NAME + " (" +
                 StandingContract.StandingEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
 
-                StandingContract.StandingEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
                 StandingContract.StandingEntry.COLUMN_DATE + " INTEGER NOT NULL, " +
-                StandingContract.StandingEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
-                StandingContract.StandingEntry.COLUMN_STANDING_ID + " INTEGER NOT NULL, " +
-
                 StandingContract.StandingEntry.COLUMN_START_TIME + " INTEGER NOT NULL, " +
                 StandingContract.StandingEntry.COLUMN_STOP_TIME + " INTEGER NOT NULL, " +
                 StandingContract.StandingEntry.COLUMN_STANDING_TIME + " INTEGER NOT NULL, " +
                 StandingContract.StandingEntry.COLUMN_SET_RECORD_TIME + " INTEGER NOT NULL, " +
 
-                " FOREIGN KEY (" + StandingContract.StandingEntry.COLUMN_LOC_KEY + ") REFERENCES " +
-                StandingContract.LocationEntry.TABLE_NAME + " (" + StandingContract.LocationEntry._ID + "), " +
+                StandingContract.StandingEntry.COLUMN_STREET_NAME + " TEXT NOT NULL," +
+                StandingContract.StandingEntry.COLUMN_STATE_NAME + " TEXT NOT NULL," +
+                StandingContract.StandingEntry.COLUMN_COUNTRY_NAME + " TEXT NOT NULL," +
+                StandingContract.StandingEntry.COLUMN_COORD_LAT + " REAL NOT NULL," +
+                StandingContract.StandingEntry.COLUMN_COORD_LONG + " REAL NOT NULL" +
+                " );";
 
-                " UNIQUE (" + StandingContract.StandingEntry.COLUMN_DATE + ", " +
-                StandingContract.StandingEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
-
-        sqLiteDatabase.execSQL(SQL_CREATE_LOCATION_TABLE);
         sqLiteDatabase.execSQL(SQL_CREATE_STANDING_TABLE);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + StandingContract.LocationEntry.TABLE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + StandingContract.StandingEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
