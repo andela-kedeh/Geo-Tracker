1d7bc7ca53e0746b17cbaee21fff8a95126398d5
andela.kedeh <kingsley.edeh@andela.co>
2015-10-19 09:22:51 +0100

    added formatter class for date and time

---
+++ app/src/main/java/com/checkpoint4/wecking/standingstillapp/util/Formater.java
@@ -0,0 +1,26 @@
+package com.checkpoint4.wecking.standingstillapp.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by andela on 10/17/15.
+ */
+public class Formater {
+    public static String timeFormater(long longDate){
+        Date date = new Date(longDate);
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss:SSS");
+        String dateFormatted = formatter.format(date);
+
+        return dateFormatted;
+    }
+
+    public static String formatDate(long longDate){
+        Date date=new Date(longDate);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        String dateText = dateFormat.format(date);
+
+        return dateText;
+    }
+}
