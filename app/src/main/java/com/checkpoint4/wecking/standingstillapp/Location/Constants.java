b77b916863930fbb2997d6245c6145323007aef2
andela.kedeh <kingsley.edeh@andela.co>
2015-11-08 15:05:03 +0100

    removed the static interval variable from Constants

--- app/src/main/java/com/checkpoint4/wecking/standingstillapp/Location/Constants.java
+++ app/src/main/java/com/checkpoint4/wecking/standingstillapp/Location/Constants.java
@@ -6,8 +6,13 @@ package com.checkpoint4.wecking.standingstillapp.Location;
 
 import android.content.Context;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
-import android.util.Log;
+import android.location.Location;
+import android.location.LocationManager;
+
+import com.checkpoint4.wecking.standingstillapp.ApplicationComponent.CircleTimerView;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
 import java.util.Locale;
@@ -23,7 +28,7 @@ public final class Constants {
 
     public static final String RECEIVER = PACKAGE_NAME + ".StandingService";
 
-    public static int interval = 5;
+    public static CircleTimerView circularTimerView;
 
     public static String getLocationAddress(Double longitude, Double latitude, int addressId, Context context){
         Geocoder gcd = new Geocoder(context.getApplicationContext(), Locale.getDefault());
@@ -35,6 +40,7 @@ public final class Constants {
         }
         return addresses.get(0).getAddressLine(addressId).toString();
     }
+
 
 
 }
